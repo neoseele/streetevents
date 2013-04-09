2e57f2f1bbb933efa7bf795128641b04104341a0
nmiao <nmiao@redhat.com>
2013-04-09 15:46:58 +1000

    add download.sh

---
+++ download.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function usage() {
+  echo "Usage: `basename $0` <input>"
+  exit 1
+}
+
+[ -z "$1" ] && usage
+
+for l in `grep -v "^#" $1`; do
+  # 2007-09|http://xxxx
+  arr=(`echo $l | tr "|" " "`)
+
+  dir=${arr[0]} # 2007-09
+  url=${arr[1]} # http://xxx
+
+  # create dir if not exist
+  [ -d "$dir" ] || mkdir $dir
+
+  # download
+  wget -P $dir -nc --content-disposition $url
+done
